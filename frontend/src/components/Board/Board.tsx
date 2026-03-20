87e53be4dd1b528167deea29d34b2049682c2722
Thomas Yeung <yeung.thomas.h@gmail.com>
2026-03-19 20:38:07 -0700

    modifying board

--- frontend/src/components/Board/Board.tsx
+++ frontend/src/components/Board/Board.tsx
@@ -1,20 +1,70 @@
+import { useEffect, useState } from "react";
 import styles from "./Board.module.css";
 
-export interface BoardData {
+type TileProps = {
+  letter: string;
+  idx: number;
+  updateSelectedTile: (idx: number) => void;
+  isSelected: boolean;
+};
+
+type BoardProps = {
+  board: BoardData;
+  boardLetters: string;
+  setBoardLetters: React.Dispatch<React.SetStateAction<string>>;
+};
+
+interface BoardData {
   width: number;
   height: number;
   letters: string;
 }
 
-function Tile({ letter }: { letter: string }) {
+function Tile({ letter, idx, updateSelectedTile, isSelected }: TileProps) {
   return (
-    <div className={styles.tile}>
+    <div
+      className={`${styles.tile} ${isSelected ? styles.selectedTile : ""}`}
+      onClick={() => {
+        updateSelectedTile(idx);
+      }}
+    >
       <span className={styles.tileLetter}>{letter}</span>
     </div>
   );
 }
 
-export function Board({ board }: { board: BoardData }) {
+export function Board({ board, boardLetters, setBoardLetters }: BoardProps) {
+  const [selectedTile, setSelectedTile] = useState(-1);
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (selectedTile === -1) {
+        return;
+      }
+
+      let newChar;
+      if (/^[a-zA-Z]$/.test(e.key)) {
+        newChar = e.key;
+      } else if (e.key === "Backspace") {
+        newChar = " ";
+      } else {
+        return;
+      }
+
+      setBoardLetters(
+        boardLetters.substring(0, selectedTile) +
+          newChar +
+          boardLetters.substring(selectedTile + 1),
+      );
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [selectedTile]);
+
   return (
     <div
       className={styles.board}
@@ -23,7 +73,15 @@ export function Board({ board }: { board: BoardData }) {
       }}
     >
       {[...board.letters].map((letter, i) => (
-        <Tile key={i} letter={letter.toUpperCase()} />
+        <Tile
+          key={i}
+          letter={letter.toUpperCase()}
+          idx={i}
+          updateSelectedTile={(idx: number) => {
+            setSelectedTile(selectedTile === idx ? -1 : idx);
+          }}
+          isSelected={selectedTile === i}
+        />
       ))}
     </div>
   );
